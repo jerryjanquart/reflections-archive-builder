97089770f93d360ba5e4417369e7e4196631af1d
Jerry Janquart <jmjanquart@gmail.com>
2026-05-01 12:02:54 -0500

    fix typo in migration

--- database/migrations/2026_05_01_151301_create_reflections_sources_table.php
+++ database/migrations/2026_05_01_151301_create_reflections_sources_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('reflections_sources', function (Blueprint $table) {
+        Schema::create('reflection_sources', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
             $table->string('url')->unique();
